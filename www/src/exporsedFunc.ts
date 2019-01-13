b4ed3bf97c64f6e65694deddeb7139bae460deb2
mironal <miro.gunp@gmail.com>
2019-01-13 17:52:34 +0900

    add log

--- www/src/exporsedFunc.ts
+++ www/src/exporsedFunc.ts
@@ -6,6 +6,9 @@ export function runCommand(
   command: string,
   opt?: ExecOptions,
 ): Promise<{ stdout: string; stderr: string }> {
+  if (opt && opt.cwd === "Globals") {
+    console.warn("maybe bug...")
+  }
   if (win.runCommand) {
     return win.runCommand(command, opt)
   }
